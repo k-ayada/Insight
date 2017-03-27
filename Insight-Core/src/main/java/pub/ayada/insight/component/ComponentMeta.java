9d1eeea85cc1bacff5a2028c13ac0b9ba2b138df
k-ayada <kiran14n@gmail.com>
2017-03-27 15:16:24 +0530

    Create ComponentMeta.java

---
+++ Insight-Core/src/main/java/pub/ayada/insight/component/ComponentMeta.java
@@ -0,0 +1,13 @@
+package pub.ayada.insight.component;
+
+public class ComponentMeta {
+
+	Object componentProperties;
+	public String getInitClassName() {
+		return null;
+	}
+	public Object getComponentProperties(){
+		return this.componentProperties;
+	}
+
+}
